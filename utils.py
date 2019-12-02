71209eef8b9a3347ecfbc0f30d2a14f582c0ebb9
gwiedemann <gwiedemann@informatik.uni-hamburg.de>
2019-12-02 14:17:58 +0100

    dev set clusters

--- utils.py
+++ utils.py
@@ -11,6 +11,10 @@ from sklearn.metrics import matthews_corrcoef, f1_score
 
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
+    
+from transformers import BertPreTrainedModel, BertModel
+import torch
+from torch import nn
 
 logger = logging.getLogger(__name__)
 csv.field_size_limit(2147483647)
@@ -107,8 +111,87 @@ class SameSideProcessor(DataProcessor):
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
+    
+
+class UkpSamProcessor(SameSideProcessor):
+    """Processor for the UKP SAM data set"""
+
+    def _create_examples(self, items, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        
+        for (i, item) in enumerate(items):
+            guid = "%s-%s" % (set_type, i)
+            text_a = item[0]
+            label = item[1]
+            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+        return examples
 
 
+
+class BertForBinaryClassification(BertPreTrainedModel):
+    r"""
+        **labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in ``[0, ..., config.num_labels - 1]``.
+            If ``config.num_labels == 1`` a regression loss is computed (Mean-Square loss),
+            If ``config.num_labels > 1`` a classification loss is computed (Cross-Entropy).
+    Outputs: `Tuple` comprising various elements depending on the configuration (config) and inputs:
+        **loss**: (`optional`, returned when ``labels`` is provided) ``torch.FloatTensor`` of shape ``(1,)``:
+            Classification (or regression if config.num_labels==1) loss.
+        **logits**: ``torch.FloatTensor`` of shape ``(batch_size, config.num_labels)``
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        **hidden_states**: (`optional`, returned when ``config.output_hidden_states=True``)
+            list of ``torch.FloatTensor`` (one for the output of each layer + the output of the embeddings)
+            of shape ``(batch_size, sequence_length, hidden_size)``:
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        **attentions**: (`optional`, returned when ``config.output_attentions=True``)
+            list of ``torch.FloatTensor`` (one for each layer) of shape ``(batch_size, num_heads, sequence_length, sequence_length)``:
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention heads.
+    Examples::
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+        input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+        labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
+        outputs = model(input_ids, labels=labels)
+        loss, logits = outputs[:2]
+    """
+    def __init__(self, config):
+        super(BertForBinaryClassification, self).__init__(config)
+        self.num_labels = config.num_labels
+        # print(config)
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        # self.classifier = nn.Sequential(
+        #     nn.Linear(config.hidden_size, config.hidden_size),
+        #     nn.ReLU(),
+        #     nn.Dropout(p=0.1),
+        #     nn.Linear(config.hidden_size, config.num_labels)
+        # )
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None,
+                position_ids=None, head_mask=None):
+        outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
+                            attention_mask=attention_mask, head_mask=head_mask)
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        
+        if labels is not None:
+            loss_fct = torch.nn.BCEWithLogitsLoss()
+            loss = loss_fct(logits.view(-1), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+
+    
+    
 def convert_example_to_feature(example_row, pad_token=0,
 sequence_a_segment_id=0, sequence_b_segment_id=1,
 cls_token_segment_id=1, pad_token_segment_id=0,
@@ -240,13 +323,16 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
 
 
 processors = {
-    "binary": SameSideProcessor
+    "binary": SameSideProcessor,
+    "ukp_sam": UkpSamProcessor
 }
 
 output_modes = {
-    "binary": "classification"
+    "binary": "classification",
+    "ukp_sam": "classification"
 }
 
 GLUE_TASKS_NUM_LABELS = {
-    "binary": 2
+    "binary": "1",
+    "ukp_sam": "2"
 }
